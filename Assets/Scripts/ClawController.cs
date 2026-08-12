e27bebdd3e7d49501c67ec5bfcddd81b8ad9bc94
Michael R <mikeremo@gmail.com>
2026-08-12 04:09:10 -0700

    WIP UAP machine prototype 2

---
+++ Assets/Scripts/ClawController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ClawController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // TO DO
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TO DO
+    }
+}
