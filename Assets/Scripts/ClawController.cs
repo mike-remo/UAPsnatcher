83bbf25c33a8f580c44cbcd9551951a2e9277516
Michael R <mikeremo@gmail.com>
2026-08-18 01:21:54 -0700

    Refactor code more, size adjustments to prototype2 prefab, fix claw arm to extend/retract with claw vertical movement

--- Assets/Scripts/ClawController.cs
+++ Assets/Scripts/ClawController.cs
@@ -4,8 +4,8 @@ using UnityEngine.InputSystem;
 public class ClawController : MonoBehaviour
 {
     [SerializeField] private InputAction playerMove, playerAction1;
-    [SerializeField] private float speedH, speedV, speedC, rangeYmin, rangeYmax;
-    [SerializeField] private float elevation, angle, openAngle, closeAngle;
+    [SerializeField] private float speedH, speedV, speedC, rangeYmin, rangeYmax, elevation;
+    [SerializeField] private float circum, length, offset, angle, openAngle, closeAngle;
     [SerializeField] private GameObject clawArm, claw1, claw2, claw3, claw4;
     private Rigidbody clawRB;
     void Awake()
@@ -42,13 +42,15 @@ public class ClawController : MonoBehaviour
     {
         speedH = 100f; // Horizontal movement speed
         speedV = 0.5f; // Vertical movement speed
-        speedC = 180f; // Claw grab speed
-        rangeYmax = 0.3f;
-        rangeYmin = -0.6f;
-        elevation = 0.3f; // Initial claw height
-        angle = 0;
-        openAngle = -20; // How much to open claw
-        closeAngle = 10; // How much to close claw
+        speedC = 180f; // Grab speed
+        rangeYmax = transform.localPosition.y; // Highest pos
+        rangeYmin = transform.localPosition.y - 0.95f; // Lowest pos
+        elevation = rangeYmax; // Initial height
+        length = clawArm.transform.localScale.y; // ARM initial length
+        circum = clawArm.transform.localScale.x; // ARM initial circumference
+        angle = 0; // PINCERS initial angle
+        openAngle = -25; // How much to open claw
+        closeAngle = 5; // How much to close claw
     }
 
     void Update()
@@ -56,46 +58,69 @@ public class ClawController : MonoBehaviour
         float x = transform.localPosition.x;
         float y = transform.localPosition.y;
         float z = transform.localPosition.z;
-        ControlsMove(x,y,z);
-        ControlsGrab(x,z);
+        float x2 = clawArm.transform.localPosition.x;
+        float y2 = clawArm.transform.localPosition.y;
+        float z2 = clawArm.transform.localPosition.z;
+        ControlsMoveH(x, y, z);
+        ControlsMoveV(x, z, x2, y2, z2);
+        ControlsGrab();
     }
 
-    void ControlsMove(float x, float y, float z) // Controls claw horizontal movement
+    void ControlsMoveH(float x, float y, float z) // Claw horizontal movement
     {
         // Movement from 2d input converted to 3d local space
         Vector2 input = playerMove.ReadValue<Vector2>();
         Vector3 input2 = new Vector3(input.x, 0, input.y);
-        if (input2.magnitude > 1f) { input2.Normalize(); }
+        input2.Normalize();
         clawRB.linearVelocity = input2 * Time.deltaTime * speedH;
     }
 
-    void ControlsGrab(float x, float z) // Controls claw vertical movement and grab action
+    void ControlsMoveV(float x, float z, float x2, float y2, float z2) // Claw vertical movement
     {
         if (playerAction1.IsPressed())
         {
-            if (elevation > rangeYmin) // Lower and open claw
+            if (elevation > rangeYmin) // Lower claw and extend arm
             {
                 elevation -= Time.deltaTime * speedV;
+                offset = Time.deltaTime * speedV / 2;
                 transform.localPosition = new Vector3(x, elevation, z);
+                length += Time.deltaTime * (speedV / 2f);
+                clawArm.transform.localScale = new Vector3(circum, length, circum);
+                clawArm.transform.localPosition = new Vector3(x2, y2 + offset, z2);
             }
-            if (angle > openAngle) { angle -= Time.deltaTime * speedC; }
-            claw1.transform.localEulerAngles = new Vector3(45, 0, angle);
-            claw2.transform.localEulerAngles = new Vector3(315, 0, angle);
-            claw3.transform.localEulerAngles = new Vector3(135, 0, angle);
-            claw4.transform.localEulerAngles = new Vector3(225, 0, angle);
         }
         else
         {
-            if (elevation < rangeYmax) // Raise and close claw
+            if (elevation < rangeYmax) // Raise claw and retract arm
             {
                 elevation += Time.deltaTime * speedV;
+                offset = Time.deltaTime * speedV / 2;
                 transform.localPosition = new Vector3(x, elevation, z);
+                length -= Time.deltaTime * (speedV / 2f);
+                clawArm.transform.localScale = new Vector3(circum, length, circum);
+                clawArm.transform.localPosition = new Vector3(x2, y2 - offset, z2);
             }
+        }
+    }
+
+    void ControlsGrab() // Controls claw open and close
+    {
+        if (playerAction1.IsPressed())
+        {
+            if (angle > openAngle) { angle -= Time.deltaTime * speedC; }
+            claw1.transform.eulerAngles = new Vector3(0, 45, angle);
+            claw2.transform.eulerAngles = new Vector3(0, 315, angle);
+            claw3.transform.eulerAngles = new Vector3(0, 135, angle);
+            claw4.transform.eulerAngles = new Vector3(0, 225, angle);
+        }
+        else
+        {
             if (angle < closeAngle) { angle += Time.deltaTime * speedC * 2; }
-            claw1.transform.localEulerAngles = new Vector3(45, 0, angle);
-            claw2.transform.localEulerAngles = new Vector3(315, 0, angle);
-            claw3.transform.localEulerAngles = new Vector3(135, 0, angle);
-            claw4.transform.localEulerAngles = new Vector3(225, 0, angle);
+            claw1.transform.eulerAngles = new Vector3(0, 45, angle);
+            claw2.transform.eulerAngles = new Vector3(0, 315, angle);
+            claw3.transform.eulerAngles = new Vector3(0, 135, angle);
+            claw4.transform.eulerAngles = new Vector3(0, 225, angle);
         }
     }
 }
+// END
--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -3,10 +3,10 @@ using UnityEngine.InputSystem;
 
 public class JoystickController : MonoBehaviour
 {
-    [SerializeField] private InputAction playerMove, playerAction1;
+    [SerializeField] private InputAction playerMove, playerAction1, playerAction2;
     [SerializeField] private float maxAngle;
-    [SerializeField] private GameObject button;
-    private Vector3 pushed, unpushed;
+    [SerializeField] private GameObject button1, button2;
+    private Vector3 pushed1, unpushed1, pushed2, unpushed2;
     
     void Awake()
     {
@@ -22,45 +22,62 @@ public class JoystickController : MonoBehaviour
             .With("Right", "<Keyboard>/rightArrow");
         
         playerAction1 = new InputAction("Activate", InputActionType.Button, "<Keyboard>/space");
+        playerAction2 = new InputAction("Alternate", InputActionType.Button, "<Keyboard>/f");
     }
 
     void OnEnable()
     {
         playerMove.Enable();
         playerAction1.Enable();
+        playerAction2.Enable();
     }
 
     void OnDisable()
     {
         playerMove.Disable();
         playerAction1.Disable();
+        playerAction2.Disable();
     }
 
     void Start()
     {
         maxAngle = 20f;
-        pushed = new Vector3(button.transform.localPosition.x,
-                             button.transform.localPosition.y - 0.015f,
-                             button.transform.localPosition.z);
-        unpushed = new Vector3(button.transform.localPosition.x,
-                               button.transform.localPosition.y,
-                               button.transform.localPosition.z);
+        pushed1 = new Vector3(button1.transform.localPosition.x,
+                             button1.transform.localPosition.y - 0.0125f,
+                             button1.transform.localPosition.z);
+        unpushed1 = new Vector3(button1.transform.localPosition.x,
+                               button1.transform.localPosition.y,
+                               button1.transform.localPosition.z);
+        pushed2 = new Vector3(button2.transform.localPosition.x,
+                             button2.transform.localPosition.y - 0.0125f,
+                             button2.transform.localPosition.z);
+        unpushed2 = new Vector3(button2.transform.localPosition.x,
+                               button2.transform.localPosition.y,
+                               button2.transform.localPosition.z);
     }
 
     void Update()
     {
-        Controls();
+        ControlsJoystick();
+        ControlsButtons();
     }
 
-    void Controls()
+    void ControlsJoystick() // Mimic joystick controls
     {
         Vector2 input = playerMove.ReadValue<Vector2>();
+        input.Normalize();
         float x = input.x * maxAngle;
         float y = 90;
         float z = input.y * maxAngle;
         transform.localEulerAngles = new Vector3(x, y, z);
+    }
 
-        if (playerAction1.IsPressed()) { button.transform.localPosition = pushed; }
-        else { button.transform.localPosition = unpushed; }
+    void ControlsButtons() // Mimic button controls
+    {
+        if (playerAction1.IsPressed()) { button1.transform.localPosition = pushed1; }
+        else { button1.transform.localPosition = unpushed1; }
+        if (playerAction2.IsPressed()) { button2.transform.localPosition = pushed2; }
+        else { button2.transform.localPosition = unpushed2; }
     }
 }
+// END
