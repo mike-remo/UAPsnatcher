a575aa784f5aeb1a8db3672e538c4a854dd0e99e
Michael R <mikeremo@gmail.com>
2026-08-30 06:09:29 -0700

    Fix missing scripts. Added random torque to prizes on start to give slight random landing position on start of game.

--- Assets/Scripts/Prize.cs
+++ Assets/Scripts/Prize.cs
@@ -4,4 +4,17 @@ public class Prize : MonoBehaviour
 {
     public int pointValue = 5;
     public bool won = false;
+    private Rigidbody rB;
+    private float forceX, forceY, forceZ, maxRange;
+    private Vector3 randomTorque;
+    void Start()
+    {   // Prizes get a slight random torque to randomize landing position
+        rB = GetComponent<Rigidbody>();
+        maxRange = 0.005f;
+        forceX = Random.Range(0f, maxRange);
+        forceY = Random.Range(0f, maxRange);
+        forceZ = Random.Range(0f, maxRange);
+        randomTorque = new Vector3(forceX, forceY, forceZ);
+        rB.AddTorque(randomTorque, ForceMode.Impulse);
+    }
 } // END
